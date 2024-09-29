7b8b7e12d92459e43c09ba7dca9506f6566fb05d
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-09-28 19:23:02 -0600

    update file

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
-from flask_cli import FlaskCLI
 import click
 from app import inicializar_app
 
 app = inicializar_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
