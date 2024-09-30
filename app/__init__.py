040a13c5ad92def33d2acc47ddb79497b29f009e
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-09-29 21:04:11 -0600

    add files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #print(request.method)
+        #print(request.form['usuario'])
+        #print(request.form['password'])
+        if request.form['usuario'] == 'admin' and request.form['password'] == '123456':
+            return redirect(url_for('index'))
+        else:
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
 
 def pagina_no_encontrada(error):
     return render_template('errores/404.html'), 404
